984b18ca5cbfe71ae43155d3a63e122199c30729
P4T4R <guitarisrockin@hotmail.com>
2015-08-17 09:07:28 -0700

    Made improvements to the frame buffer

--- frame_buf_alt.v
+++ frame_buf_alt.v
@@ -7,12 +7,12 @@ Spring 2015 / Fall 2015
 --------------------------------------------------
 
 Frame Buffer for the Altera External Memory Interface
-Authors: Padraic Hagerty (guitarisrockin@hotmail.com)
+Authors:	Padraic Hagerty (guitarisrockin@hotmail.com)
 
 Description:
 	This is the frame buffer designed for the Altera Cyclone 5 GX Starter Kit.
-	It autonomously handles all addressing to the memory based upon the rd_en_in
-	and wr_en_in signals.
+	It autonomously handles all addressing to the memory based upon the rd_en
+	and wr_en signals.
 	
 Instructions:
 	Look at frame_buf_alt_tb.v for an example of how this module should be
@@ -26,20 +26,21 @@ module frame_buf_alt #(
 				ADDR_WIDTH = 29,
 				MEM_DEPTH = 1 << ADDR_WIDTH,
 				BASE_ADDR = 2,
-				BUF_SIZE = 307200
+				BUF_SIZE = 307200				// 640 * 480 pixels
 )(
 	input							wr_clk,
 									rd_clk,
 									reset,
-									wr_en_in,
-									rd_en_in,
-									wr_rdy,
-									rd_rdy,
-	output	reg						wr_en,
+									wr_en,
 									rd_en,
+									ram_rdy,
+									avl_ready,
+	output	reg						avl_write_req,
+									avl_read_req,
 									full,
 	output	reg	[ADDR_WIDTH - 1:0]	wr_addr,
-									rd_addr
+									rd_addr,
+	output		[ADDR_WIDTH - 1:0]	avl_addr
 );
 
 	localparam
@@ -53,41 +54,45 @@ module frame_buf_alt #(
 		FILL = 1'h1,
 		READ = 1'h1;
 	
-	reg	mem_rdy = 1'b0;
+	reg						mem_rdy = 1'b0;
 	(* syn_encoding = "safe" *)
-	reg	curr_state = IDLE,
-		rd_curr_state = IDLE;
-	reg	rd_data_valid_reg,
-		wr_c = 1'b0,
-		rd_c = 1'b0,
-		rd_done = DEASSERT_H;
+	reg						curr_state = IDLE,
+							rd_curr_state = IDLE;
+	reg						rd_data_valid_reg,
+							wr_c = 1'b0,
+							rd_c = 1'b0,
+							rd_done = DEASSERT_H;
+		
+	assign avl_addr = (wr_en) ? rd_addr : wr_addr;
 	
 	always @(posedge wr_clk) begin
 		if (~reset) begin
 		
 			curr_state <= IDLE;
 			wr_addr <= BASE_ADDR;
-			wr_en <= DEASSERT_L;
 			mem_rdy <= DEASSERT_H;
 			wr_c <= 1'b0;
 			full <= DEASSERT_H;
+			avl_write_req <= DEASSERT_H;
 			
-		end else
+		end else if (ram_rdy)
 		
 			case (curr_state)
 				IDLE: begin
-					if (wr_en_in == ASSERT_L && ((wr_addr >= rd_addr &&
-							rd_c == wr_c) || (wr_addr < rd_addr &&
-							rd_c != wr_c))) begin
+					if (wr_en == ASSERT_L && avl_ready &&
+							((wr_addr >= rd_addr && rd_c == wr_c) ||
+							(wr_addr < rd_addr && rd_c != wr_c))) begin
 							
 						curr_state <= FILL;
-						wr_en <= ASSERT_L;
+						//wr_en <= ASSERT_L;
+						avl_write_req <= ASSERT_H;
 						full <= DEASSERT_H;
 						
 					end else begin
 					
 						curr_state <= IDLE;
-						wr_en <= DEASSERT_L;
+						//wr_en <= DEASSERT_L;
+						avl_write_req <= DEASSERT_H;
 						
 						if (rd_done)
 							full <= DEASSERT_H;
@@ -100,31 +105,34 @@ module frame_buf_alt #(
 						curr_state <= IDLE;
 						wr_addr <= BASE_ADDR;
 						wr_c <= ~wr_c;
-						wr_en <= DEASSERT_L;
+						//wr_en <= DEASSERT_L;
+						avl_write_req <= DEASSERT_H;
 						full <= ASSERT_H;
 						
-					end else if (wr_en_in == ASSERT_L && ((wr_addr >= rd_addr &&
-										rd_c == wr_c) || (wr_addr < rd_addr &&
-										rd_c != wr_c))) begin
+					end else if (wr_en == ASSERT_L && avl_ready &&
+									((wr_addr >= rd_addr && rd_c == wr_c) ||
+									(wr_addr < rd_addr && rd_c != wr_c))) begin
 												
 						curr_state <= FILL;
 						mem_rdy <= 1'b1;
-						wr_en <= ASSERT_L;
+						//wr_en <= ASSERT_L;
+						avl_write_req <= ASSERT_H;
 						
-						if (wr_rdy)
+						/*if (wr_rdy)
 							if (wr_addr == BASE_ADDR + BUF_SIZE) begin
 								curr_state <= IDLE;
 								wr_addr <= BASE_ADDR;
 								wr_c <= ~wr_c;
-								wr_en <= DEASSERT_L;
+								//wr_en <= DEASSERT_L;
 								full <= ASSERT_H;
-							end else
+							end else*/
 								wr_addr <= wr_addr + 1;
 								
 					end else begin
 					
 						curr_state <= FILL;
-						wr_en <= DEASSERT_L;
+						//wr_en <= DEASSERT_L;
+						avl_write_req <= DEASSERT_H;
 						
 					end
 					
@@ -137,27 +145,30 @@ module frame_buf_alt #(
 		if (~reset) begin
 		
 			rd_curr_state <= IDLE;
-			rd_en <= DEASSERT_L;
 			rd_addr <= BASE_ADDR;
 			rd_c <= 1'b0;
 			rd_done <= DEASSERT_H;
+			avl_read_req <= DEASSERT_H;
 			
-		end else
+		end else if (ram_rdy)
 		
 			case (rd_curr_state)
 				IDLE: begin
-					if (rd_en_in == ASSERT_L && mem_rdy == 1'b1 &&
+					if (rd_en == ASSERT_L && mem_rdy == 1'b1 &&
+							wr_en == DEASSERT_L && avl_ready &&
 							((rd_addr < wr_addr && rd_c == wr_c) ||
 							(rd_addr >= wr_addr && rd_c != wr_c))) begin
 							
 						rd_curr_state <= READ;
-						rd_en <= ASSERT_L;
+						//rd_en <= ASSERT_L;
+						avl_read_req <= ASSERT_H;
 						rd_done <= DEASSERT_H;
 						
 					end else begin
 					
 						rd_curr_state <= IDLE;
-						rd_en <= DEASSERT_L;
+						//rd_en <= DEASSERT_L;
+						avl_read_req <= DEASSERT_H;
 						
 					end
 				end
@@ -168,29 +179,34 @@ module frame_buf_alt #(
 						rd_curr_state <= IDLE;
 						rd_addr <= BASE_ADDR;
 						rd_c <= ~rd_c;
-						rd_en <= DEASSERT_L;
+						//rd_en <= DEASSERT_L;
+						avl_read_req <= DEASSERT_H;
 						rd_done <= ASSERT_H;
 						
-					end else if (rd_en_in == ASSERT_L && ((rd_addr < wr_addr &&
-										rd_c == wr_c) || (rd_addr >= wr_addr &&
-										rd_c != wr_c))) begin
+					end else if (rd_en == ASSERT_L && wr_en == DEASSERT_L &&
+									avl_ready &&
+									((rd_addr < wr_addr && rd_c == wr_c) ||
+									(rd_addr >= wr_addr && rd_c != wr_c)))
+																		begin
 						
 						rd_curr_state <= READ;
-						rd_en <= ASSERT_L;
+						//rd_en <= ASSERT_L;
+						avl_read_req <= ASSERT_H;
 						
-						if (rd_rdy)
+						/*if (rd_rdy)
 							if (rd_addr == BASE_ADDR + BUF_SIZE) begin
 								rd_curr_state <= IDLE;
 								rd_addr <= BASE_ADDR;
 								rd_c <= ~rd_c;
-								rd_en <= DEASSERT_L;
+								//rd_en <= DEASSERT_L;
 								rd_done <= ASSERT_H;
-							end else
+							end else*/
 								rd_addr <= rd_addr + 1;
 					
 					end else begin
 						rd_curr_state <= READ;
-						rd_en <= DEASSERT_L;
+						//rd_en <= DEASSERT_L;
+						avl_read_req <= DEASSERT_H;
 					end
 					
 				end
